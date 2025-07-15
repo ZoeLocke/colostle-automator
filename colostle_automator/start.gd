eef50d611ec47c3bfd876b00df6f3bfb532e61d2
ZoeLocke <oz.locke@gmail.com>
2025-07-15 21:41:42 +0100

    adjusted layout and scaling options, app is now responsive

--- colostle_automator/start.gd
+++ colostle_automator/start.gd
@@ -2,15 +2,24 @@ extends Control
 # Shorthands for objects
 var options
 var text_box
+var shuffle_button
+var draw_button
 var clear_log_popup = preload("res://clear_log_popup.tscn")
 
 func _ready():
-	options = $VBoxContainer/HBoxContainer/CoreUI/ButtonContainer/HBoxContainer/OptionButton
-	text_box = $VBoxContainer/HBoxContainer/CoreUI/TextBoxContainer/TextBox
+	options = $MarginContainer/VBoxContainer/OptionButton
+	text_box = $MarginContainer/VBoxContainer/TextBox
+	shuffle_button = $MarginContainer/VBoxContainer/ButtonMarginContainer/HBoxContainer/ShuffleButton
+	draw_button = $MarginContainer/VBoxContainer/ButtonMarginContainer/HBoxContainer/DrawButton
+	
 	# Set an initial selection for the dropdown
 	options.select(1)
 
 func draw_card():
+	# Make sure there are still cards in the deck, if not shuffle it
+	if Globals.deck.is_empty():
+		text_box.append_text("[p align=center][color=dark_green][i]The deck was empty, you have shuffled the deck.[/i][/color][/p]\n\n")
+		Globals.shuffle()
 	# Get the option selected so we know what to return from the card
 	var selected = options.get_selected_id()
 	var lookup = options.get_item_text(selected)
@@ -23,11 +32,12 @@ func draw_card():
 func _on_draw_button_up() -> void:
 	draw_card()
 	Globals.first_result = false
+	draw_button.release_focus()
 
 func _on_shuffle_button_up() -> void:
 	Globals.shuffle()
 	text_box.append_text("[p align=center][color=dark_green][i]You have shuffled the deck[/i][/color][/p]\n\n")
-	$VBoxContainer/HBoxContainer/ExtrasMarginContainer/Extras/ShuffleButton.release_focus()
+	shuffle_button.release_focus()
 	
 func _on_clear_button_up() -> void:
 	var popup = clear_log_popup.instantiate()
